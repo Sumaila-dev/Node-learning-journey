ab2b894415b1e5f803c0a53b0ddf5c1e92cdd075
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-06-15 13:16:10 +0100

    The http files
    Uses modules from other files

--- MODULES-EXAMPLES/http.js
+++ MODULES-EXAMPLES/http.js
@@ -1,3 +1,11 @@
-function request(url, data) {
+const request = require('./request.js')
+const response = require('./response.js')
+
+function makeRequest(url, data) {
+    request.send(url, data);
+    return response.read();
 
 }
+
+const responseData = makeRequest('https://google.com', 'hello');
+console.log(responseData)
